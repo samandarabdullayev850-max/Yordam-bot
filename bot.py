0ac93d511a7cda4017c1f475bbcdb0f8a75dd4f1
samandarabdullayev850-max <samandarabdullayev850@gmail.com>
2026-04-13 12:17:29 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ from datetime import datetime
 TOKEN = "8381516564:AAHBCKfeR7wy3SQf6ntwsSUVAS1gsvZ1R0o"
 GROQ = "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"
 ADMIN = "8726418671"
-SHEETS_URL = "https://script.google.com/macros/s/AKfycbzRnJbIKgsjpTb8SfNv6nJ4yfzmVoyEg4pdVpQhzqavsx6bNNYrws84FXFB7bEbNEcc1g/exec"
+SHEETS_URL = "https://script.google.com/macros/s/AKfycbyqCLNmhpZ_4-7J9-d6Jt3s6qxHKpfie4emgXh_tmLGItmrFUYTET5FokrSBCw4b6nQ7g/exec"
 OBUNA_MAJBURIY = False
 CHANNEL = "@sizning_kanal"
 
